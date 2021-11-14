034eca5f300dd0da9e7f76613fdf652a2fe81ca1
FShinoda <fernanda.shinoda@hotmail.com>
2021-11-14 09:59:25 -0300

    db interaction done

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,13 @@
 ﻿import amazonbooks = require("teem");
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('../../ESPM/4oSemestre/Python/code/Inter/projeto/conexao.db', sqlite3.OPEN_READONLY, (err) =>{
+    if (err){
+        return console.log(err.message);
+    } else {
+        console.log("Conectado ao DB com sucesso!");
+    }
+});
 
 class IndexRoute {
 	public async index(req: amazonbooks.Request, res: amazonbooks.Response) {
@@ -10,7 +19,26 @@ class IndexRoute {
 	}
 	
 	public async dashboard(req: amazonbooks.Request, res: amazonbooks.Response) {
-		res.render("index/dashboard");
+		
+		var catList = []; 
+
+		(async () => {
+			try {
+			  	// Creating the Books table (Book_ID, Title, Author, Comments)
+			  	await db.all('SELECT * from Category', async (err, rows) =>{
+					if(err){
+						throw err;
+					}
+					await rows.forEach((c)=>{
+						catList.push(c)
+					})
+					res.render("index/dashboard", {cat: catList});
+				})	
+			}
+			catch (error) { throw error; }
+		  })();
+
+		
 	}
 
 	public async autores(req: amazonbooks.Request, res: amazonbooks.Response) {
--- src/js/routes/index.js
+++ src/js/routes/index.js
@@ -1,4 +1,13 @@
 "use strict";
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('../../ESPM/4oSemestre/Python/code/Inter/projeto/conexao.db', sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+        return console.log(err.message);
+    }
+    else {
+        console.log("Conectado ao DB com sucesso!");
+    }
+});
 class IndexRoute {
     async index(req, res) {
         let pageSettings = {
@@ -7,7 +16,24 @@ class IndexRoute {
         res.render("index/index", pageSettings);
     }
     async dashboard(req, res) {
-        res.render("index/dashboard");
+        var catList = [];
+        (async () => {
+            try {
+                // Creating the Books table (Book_ID, Title, Author, Comments)
+                await db.all('SELECT * from Category', async (err, rows) => {
+                    if (err) {
+                        throw err;
+                    }
+                    await rows.forEach((c) => {
+                        catList.push(c);
+                    });
+                    res.render("index/dashboard", { cat: catList });
+                });
+            }
+            catch (error) {
+                throw error;
+            }
+        })();
     }
     async autores(req, res) {
         res.render("index/authors");
