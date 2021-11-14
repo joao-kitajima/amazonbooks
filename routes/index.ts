0ef265d10ca8601a35919ab6f8190ffe2bf25fb2
FShinoda <fernanda.shinoda@hotmail.com>
2021-11-14 12:17:32 -0300

    routes added

--- routes/index.ts
+++ routes/index.ts
@@ -19,8 +19,7 @@ class IndexRoute {
 	}
 	
 	public async dashboard(req: amazonbooks.Request, res: amazonbooks.Response) {
-		
-		var catList = []; 
+		let catList = []; 
 
 		(async () => {
 			try {
@@ -32,7 +31,7 @@ class IndexRoute {
 					await rows.forEach((c)=>{
 						catList.push(c)
 					})
-					res.render("index/dashboard", {cat: catList});
+					res.render("index/dashboard", {catList: catList});
 				})	
 			}
 			catch (error) { throw error; }
@@ -42,7 +41,150 @@ class IndexRoute {
 	}
 
 	public async autores(req: amazonbooks.Request, res: amazonbooks.Response) {
-		res.render("index/authors");
+		let autList = [];
+
+		(async () => {
+			try {
+			  	// Creating the Books table (Book_ID, Title, Author, Comments)
+			  	await db.all('SELECT autName from Author', async (err, rows) =>{
+					if(err){
+						throw err;
+					}
+					await rows.forEach((a)=>{
+						autList.push(a)
+					})
+					res.render("index/authors", {autList: autList});
+				})	
+			}
+			catch (error) { throw error; }
+		  })();
+
+	}
+
+	public async livros(req: amazonbooks.Request, res: amazonbooks.Response){
+		let livList = [];
+
+		(async () => {
+			try {
+			  	// Creating the Books table (Book_ID, Title, Author, Comments)
+			  	await db.all('SELECT autName from Author', async (err, rows) =>{
+					if(err){
+						throw err;
+					}
+					await rows.forEach((a)=>{
+						livList.push(a)
+					})
+					res.render("index/books", {livList: livList});
+				})	
+			}
+			catch (error) { throw error; }
+		  })();
+	}
+
+	public async autoajuda(req: amazonbooks.Request, res: amazonbooks.Response){
+		let ajuList = [];
+
+		(async () => {
+			try {
+			  	// Creating the Books table (Book_ID, Title, Author, Comments)
+			  	await db.all('SELECT autName from Author', async (err, rows) =>{
+					if(err){
+						throw err;
+					}
+					await rows.forEach((a)=>{
+						ajuList.push(a)
+					})
+					res.render("index/selfHelp", {ajuList: ajuList});
+				})	
+			}
+			catch (error) { throw error; }
+		  })();
+	}
+
+	public async ficcao(req: amazonbooks.Request, res: amazonbooks.Response){
+		let ficList = [];
+
+		(async () => {
+			try {
+			  	// Creating the Books table (Book_ID, Title, Author, Comments)
+			  	await db.all('SELECT autName from Author', async (err, rows) =>{
+					if(err){
+						throw err;
+					}
+					await rows.forEach((a)=>{
+						ficList.push(a)
+					})
+					res.render("index/fiction", {ficList: ficList});
+				})	
+			}
+			catch (error) { throw error; }
+		  })();
+	}
+
+	public async politica(req: amazonbooks.Request, res: amazonbooks.Response){
+		let polList = [];
+
+		(async () => {
+			try {
+			  	// Creating the Books table (Book_ID, Title, Author, Comments)
+			  	await db.all('SELECT autName from Author', async (err, rows) =>{
+					if(err){
+						throw err;
+					}
+					await rows.forEach((a)=>{
+						polList.push(a)
+					})
+					res.render("index/politics", {polList: polList});
+				})	
+			}
+			catch (error) { throw error; }
+		  })();
+	}
+
+	public async academicos(req: amazonbooks.Request, res: amazonbooks.Response){
+		let acaList = [];
+
+		(async () => {
+			try {
+			  	// Creating the Books table (Book_ID, Title, Author, Comments)
+			  	await db.all('SELECT autName from Author', async (err, rows) =>{
+					if(err){
+						throw err;
+					}
+					await rows.forEach((a)=>{
+						acaList.push(a)
+					})
+					res.render("index/academics", {acaList: acaList});
+				})	
+			}
+			catch (error) { throw error; }
+		  })();
+	}
+
+	public async editoras(req: amazonbooks.Request, res: amazonbooks.Response){
+		let pubList = [];
+
+		(async () => {
+			try {
+			  	// Creating the Books table (Book_ID, Title, Author, Comments)
+			  	await db.all(`
+				  SELECT proName, proPrice, proPublisher, c.catName 
+					FROM Product p 
+					INNER JOIN Category c 
+					INNER JOIN Product_Category pc 
+					WHERE proPublisher = "Todolivro" and p.proCode = pc.proCode and c.catCode = pc.catCode ;`, 
+				  async (err, rows) =>{
+					if(err){
+						throw err;
+					}
+					await rows.forEach((a)=>{
+						pubList.push(a)
+					})
+					res.render("index/publishers", {pubList: pubList});
+				})	
+			}
+			catch (error) { throw error; }
+		  })();
 	}
 }
 
--- src/js/routes/index.js
+++ src/js/routes/index.js
@@ -16,7 +16,7 @@ class IndexRoute {
         res.render("index/index", pageSettings);
     }
     async dashboard(req, res) {
-        var catList = [];
+        let catList = [];
         (async () => {
             try {
                 // Creating the Books table (Book_ID, Title, Author, Comments)
@@ -27,7 +27,7 @@ class IndexRoute {
                     await rows.forEach((c) => {
                         catList.push(c);
                     });
-                    res.render("index/dashboard", { cat: catList });
+                    res.render("index/dashboard", { catList: catList });
                 });
             }
             catch (error) {
@@ -36,7 +36,149 @@ class IndexRoute {
         })();
     }
     async autores(req, res) {
-        res.render("index/authors");
+        let autList = [];
+        (async () => {
+            try {
+                // Creating the Books table (Book_ID, Title, Author, Comments)
+                await db.all('SELECT autName from Author', async (err, rows) => {
+                    if (err) {
+                        throw err;
+                    }
+                    await rows.forEach((a) => {
+                        autList.push(a);
+                    });
+                    res.render("index/authors", { autList: autList });
+                });
+            }
+            catch (error) {
+                throw error;
+            }
+        })();
+    }
+    async livros(req, res) {
+        let autList = [];
+        (async () => {
+            try {
+                // Creating the Books table (Book_ID, Title, Author, Comments)
+                await db.all('SELECT autName from Author', async (err, rows) => {
+                    if (err) {
+                        throw err;
+                    }
+                    await rows.forEach((a) => {
+                        autList.push(a);
+                    });
+                    res.render("index/books", { autList: autList });
+                });
+            }
+            catch (error) {
+                throw error;
+            }
+        })();
+    }
+    async autoajuda(req, res) {
+        let autList = [];
+        (async () => {
+            try {
+                // Creating the Books table (Book_ID, Title, Author, Comments)
+                await db.all('SELECT autName from Author', async (err, rows) => {
+                    if (err) {
+                        throw err;
+                    }
+                    await rows.forEach((a) => {
+                        autList.push(a);
+                    });
+                    res.render("index/selfHelp", { autList: autList });
+                });
+            }
+            catch (error) {
+                throw error;
+            }
+        })();
+    }
+    async ficcao(req, res) {
+        let autList = [];
+        (async () => {
+            try {
+                // Creating the Books table (Book_ID, Title, Author, Comments)
+                await db.all('SELECT autName from Author', async (err, rows) => {
+                    if (err) {
+                        throw err;
+                    }
+                    await rows.forEach((a) => {
+                        autList.push(a);
+                    });
+                    res.render("index/fiction", { autList: autList });
+                });
+            }
+            catch (error) {
+                throw error;
+            }
+        })();
+    }
+    async politica(req, res) {
+        let autList = [];
+        (async () => {
+            try {
+                // Creating the Books table (Book_ID, Title, Author, Comments)
+                await db.all('SELECT autName from Author', async (err, rows) => {
+                    if (err) {
+                        throw err;
+                    }
+                    await rows.forEach((a) => {
+                        autList.push(a);
+                    });
+                    res.render("index/politics", { autList: autList });
+                });
+            }
+            catch (error) {
+                throw error;
+            }
+        })();
+    }
+    async academicos(req, res) {
+        let autList = [];
+        (async () => {
+            try {
+                // Creating the Books table (Book_ID, Title, Author, Comments)
+                await db.all('SELECT autName from Author', async (err, rows) => {
+                    if (err) {
+                        throw err;
+                    }
+                    await rows.forEach((a) => {
+                        autList.push(a);
+                    });
+                    res.render("index/academics", { autList: autList });
+                });
+            }
+            catch (error) {
+                throw error;
+            }
+        })();
+    }
+    async editoras(req, res) {
+        let pubList = [];
+        (async () => {
+            try {
+                // Creating the Books table (Book_ID, Title, Author, Comments)
+                await db.all(`
+				  SELECT proName, proPrice, proPublisher, c.catName 
+					FROM Product p 
+					INNER JOIN Category c 
+					INNER JOIN Product_Category pc 
+					WHERE proPublisher = "Todolivro" and p.proCode = pc.proCode and c.catCode = pc.catCode ;`, async (err, rows) => {
+                    if (err) {
+                        throw err;
+                    }
+                    await rows.forEach((a) => {
+                        pubList.push(a);
+                    });
+                    res.render("index/publishers", { pubList: pubList });
+                });
+            }
+            catch (error) {
+                throw error;
+            }
+        })();
     }
 }
 module.exports = IndexRoute;
