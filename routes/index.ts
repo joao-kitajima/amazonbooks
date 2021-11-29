db51c1a1d98d5895a6f87b5056564a12c08cbf01
João Kitajima <joao.kitajima@outlook.com>
2021-11-29 15:01:00 -0300

    new report cars added

--- routes/index.ts
+++ routes/index.ts
@@ -18,11 +18,21 @@ class IndexRoute {
 		res.render(
 			'index/report',
 			{
+				earliest_date: await scalar('SELECT MIN(proScrapDate) FROM Product;'),
+				latest_date: await scalar('SELECT MAX(proScrapDate) FROM Product;'),
 				total_records: await scalar('SELECT COUNT(proCode) FROM Product;'),
 				total_books: await scalar('SELECT COUNT(DISTINCT autCode) FROM Author;'),
 				total_authors: await scalar('SELECT COUNT(DISTINCT proName) FROM Product;'),
 				total_publishers: await scalar('SELECT COUNT(DISTINCT proPublisher) FROM Product;'),
-				total_categories: await scalar('SELECT COUNT(DISTINCT catName) FROM Category;')
+				total_categories: await scalar('SELECT COUNT(DISTINCT catName) FROM Category;'),
+				total_reviews: await scalar('SELECT SUM(proReview) FROM (SELECT proName, MAX(proReview) AS proReview FROM Product WHERE proReview != "N/A" GROUP BY proName);'),
+				total_pages: await scalar('SELECT SUM(proPages) FROM (SELECT proName, proPages AS proPages FROM Product WHERE proPages IS NOT NULL AND proPages != "N/A" GROUP BY proName);'),
+				avg_price: await scalar('SELECT ROUND(AVG(proPrice), 2) FROM (SELECT proName, proPrice FROM Product GROUP BY proName);'),
+				max_price: await scalar('SELECT ROUND(MAX(proPrice), 2) FROM (SELECT proName, proPrice FROM Product GROUP BY proName);'),
+				min_price: await scalar('SELECT ROUND(MIN(proPrice), 2) FROM (SELECT proName, proPrice FROM Product GROUP BY proName) WHERE proPrice  >= 0;'),
+				avg_stars: await scalar('SELECT ROUND(AVG(proStar), 2) FROM (SELECT proName, proStar FROM Product WHERE proStar != "N/A" AND proStar IS NOT NULL GROUP BY proName);'),
+				max_reviews_perbook: await scalar('SELECT MAX(proReview) FROM (SELECT proName, MAX(proReview) AS proReview FROM Product WHERE proReview != "N/A" AND proReview IS NOT NULL GROUP BY proName);'),
+				avg_reviews_perbook: await scalar('SELECT CAST(ROUND(AVG(proReview)) AS INTEGER) FROM (SELECT proName, MAX(proReview) AS proReview FROM Product WHERE proReview != "N/A" AND proReview IS NOT NULL GROUP BY proName);'),
 			}
 		);
 	}
